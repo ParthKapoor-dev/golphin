c000d58c890dc996ce7c6cb435611b72d81f6f43
github-action-benchmark <github@users.noreply.github.com>
2026-08-31 15:04:07 +0000

    add Go Benchmarks (go) benchmark result for 8c138af3d2fdcf82099fe1e438cab3e2c4d70b36

---
+++ dev/bench/data.js
@@ -0,0 +1,176 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1788188647709,
+  "repoUrl": "https://github.com/ParthKapoor-dev/golphin",
+  "entries": {
+    "Go Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "parthkapoor.coder@gmail.com",
+            "name": "Parth Kapoor",
+            "username": "ParthKapoor-dev"
+          },
+          "committer": {
+            "email": "parthkapoor.coder@gmail.com",
+            "name": "Parth Kapoor",
+            "username": "ParthKapoor-dev"
+          },
+          "distinct": true,
+          "id": "8c138af3d2fdcf82099fe1e438cab3e2c4d70b36",
+          "message": "ci(benchmarks): track benchmarks using github-action-benchmark\n\nregular benchmarks should now be viewable from https://parthkapoor-dev.github.io/golphin/dev/bench/",
+          "timestamp": "2026-08-31T20:31:11+05:30",
+          "tree_id": "56770bb0aaf8ec0dcd9d118a6cf50448f377adcb",
+          "url": "https://github.com/ParthKapoor-dev/golphin/commit/8c138af3d2fdcf82099fe1e438cab3e2c4d70b36"
+        },
+        "date": 1788188646657,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkGetOldest",
+            "value": 1030,
+            "unit": "ns/op\t     144 B/op\t       4 allocs/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - ns/op",
+            "value": 1030,
+            "unit": "ns/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - B/op",
+            "value": 144,
+            "unit": "B/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - allocs/op",
+            "value": 4,
+            "unit": "allocs/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest",
+            "value": 1020,
+            "unit": "ns/op\t     144 B/op\t       4 allocs/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - ns/op",
+            "value": 1020,
+            "unit": "ns/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - B/op",
+            "value": 144,
+            "unit": "B/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - allocs/op",
+            "value": 4,
+            "unit": "allocs/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest",
+            "value": 1007,
+            "unit": "ns/op\t     144 B/op\t       4 allocs/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - ns/op",
+            "value": 1007,
+            "unit": "ns/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - B/op",
+            "value": 144,
+            "unit": "B/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkGetOldest - allocs/op",
+            "value": 4,
+            "unit": "allocs/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete",
+            "value": 8969,
+            "unit": "ns/op\t     736 B/op\t      24 allocs/op",
+            "extra": "132842 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - ns/op",
+            "value": 8969,
+            "unit": "ns/op",
+            "extra": "132842 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - B/op",
+            "value": 736,
+            "unit": "B/op",
+            "extra": "132842 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - allocs/op",
+            "value": 24,
+            "unit": "allocs/op",
+            "extra": "132842 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete",
+            "value": 12315,
+            "unit": "ns/op\t    1104 B/op\t      36 allocs/op",
+            "extra": "98320 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - ns/op",
+            "value": 12315,
+            "unit": "ns/op",
+            "extra": "98320 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - B/op",
+            "value": 1104,
+            "unit": "B/op",
+            "extra": "98320 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - allocs/op",
+            "value": 36,
+            "unit": "allocs/op",
+            "extra": "98320 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete",
+            "value": 6807,
+            "unit": "ns/op\t     490 B/op\t      16 allocs/op",
+            "extra": "177417 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - ns/op",
+            "value": 6807,
+            "unit": "ns/op",
+            "extra": "177417 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - B/op",
+            "value": 490,
+            "unit": "B/op",
+            "extra": "177417 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRandomDataGetAndDelete - allocs/op",
+            "value": 16,
+            "unit": "allocs/op",
+            "extra": "177417 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
